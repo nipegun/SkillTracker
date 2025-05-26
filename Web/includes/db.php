1a4bd5ded3b991b82a67612af5b87d45001682de
NiPeGun <nipegun@gmail.com>
2025-05-26 14:13:48 +0200

    Update db.php

--- Web/includes/db.php
+++ Web/includes/db.php
@@ -1,3 +1,9 @@
+<?php
 require_once 'config.php';
-$pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+
+$pdo = new PDO(
+  'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4',
+  DB_USER,
+  DB_PASS
+);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
