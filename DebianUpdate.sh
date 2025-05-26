1a3262e22189c0dc6855b0ddbe15abecee85a9ed
NiPeGun <nipegun@gmail.com>
2025-05-26 15:41:59 +0200

    Update DebianUpdate.sh

--- DebianUpdate.sh
+++ DebianUpdate.sh
@@ -9,7 +9,7 @@
   git clone --depth=1 https://github.com/nipegun/SkillSelector
 
 # Erase the previus web files
-  rm -rf /var/www/SkillSelector/*
+  sudo rm -rf /var/www/SkillSelector/*
 
 # Copy new files
   sudo cp -vr /tmp/SkillSelector/Web/* /var/www/SkillSelector/
