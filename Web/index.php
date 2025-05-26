3d750c6aa5803d0dc29d108061a1bd8fb309d943
NiPeGun <nipegun@gmail.com>
2025-05-26 15:47:46 +0200

    Update index.php

--- Web/index.php
+++ Web/index.php
@@ -1,5 +1,7 @@
-<form action="login.php" method="POST">
-  <label>Email: <input type="email" name="email" required></label><br>
-  <label>Contraseña: <input type="password" name="password" required></label><br>
-  <button type="submit">Ingresar</button>
-</form>
+<center>
+  <form action="login.php" method="POST">
+    <label>Email: <input type="email" name="email" required></label><br>
+    <label>Contraseña: <input type="password" name="password" required></label><br>
+    <button type="submit">Ingresar</button>
+  </form>
+</center>
