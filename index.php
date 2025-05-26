98cd7372f0223c8e61fd4359c4347a35aca8c60a
NiPeGun <nipegun@gmail.com>
2025-05-26 11:53:19 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<form action="login.php" method="POST">
+  <label>Email: <input type="email" name="email" required></label><br>
+  <label>Contraseña: <input type="password" name="password" required></label><br>
+  <button type="submit">Ingresar</button>
+</form>
