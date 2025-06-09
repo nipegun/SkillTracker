4759b61e2dd227207ddc30c9678c945edd2847dd
NiPeGun <nipegun@gmail.com>
2025-06-09 12:03:42 +0200

    Update index.php

--- Web/index.php
+++ Web/index.php
@@ -1,8 +1,15 @@
+<?php
+session_start();
+$csrf_token = bin2hex(random_bytes(32));
+$_SESSION['csrf_token'] = $csrf_token;
+?>
+
 <center>
   <p><img src="/images/SkillTrackerLogo.png" alt="Logo de SkillTracker" class="SkillTrackerLogo"></p>
   <form action="login.php" method="POST">
     <label>Email: <input type="email" name="email" required></label><br>
     <label>Contraseña: <input type="password" name="password" required></label><br>
+    <input type="hidden" name="csrf_token" value="<?= $csrf_token ?>">
     <button type="submit">Ingresar</button>
   </form>
 </center>
