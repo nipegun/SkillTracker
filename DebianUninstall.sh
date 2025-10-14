aa9fa1cb26684c7877ee8807e2f3354f460d34ba
NiPeGun <nipegun@gmail.com>
2025-10-14 22:57:25 +0200

    Update DebianUninstall.sh

--- DebianUninstall.sh
+++ DebianUninstall.sh
@@ -25,11 +25,11 @@
   sudo systemctl reload apache2
 
 # Remove /var/www folders
-  sudo rm -rf /var/www/SkillTracker/
-  sudo rm -rf /var/www/SkillTrackerLogs/
+  sudo rm -rfv /var/www/SkillTracker/
+  sudo rm -rfv /var/www/SkillTrackerLogs/
 
 # Remove SkillTracker apache configuration file
-  sudo rm -f /etc/apache2/sites-available/SkillTracker.conf
+  sudo rm -fv /etc/apache2/sites-available/SkillTracker.conf
 
 # Remove database
   sudo mysql -u root -pP@ssw0rd -e "DROP DATABASE SkillTracker;"
